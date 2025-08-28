e8e36883f5999915311353032a8a89dad67d8075
pankajsinghRajpurohit <pankajsinh11223@gmail.com>
2025-08-28 12:14:52 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [food_list, setFoodList] = useState([]);
 
-  const url = "http://localhost:4000";
+  const url = "https://fooddelivery-backend-y2yc.onrender.com";
 
   const [token, setToken] = useState("");
 
